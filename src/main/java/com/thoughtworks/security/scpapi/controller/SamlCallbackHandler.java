815e54f5a013d5ce17ffd495c1eb81499301dfdf
xpw-osr <x1npen9w@gmail.com>
2021-02-18 16:20:24 +0800

    change response to redirect

--- src/main/java/com/thoughtworks/security/scpapi/controller/SamlCallbackHandler.java
+++ src/main/java/com/thoughtworks/security/scpapi/controller/SamlCallbackHandler.java
@@ -4,12 +4,14 @@ import com.thoughtworks.security.scpapi.config.properties.JWTProperties;
 import com.thoughtworks.security.scpapi.service.UserService;
 import com.thoughtworks.security.scpapi.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +20,16 @@ public class SamlCallbackHandler {
     private final JWTProperties jwtProperties;
     private final UserService userService;
 
-    @GetMapping
-    public Optional<String> callback(@RequestParam(name = "saml_response") String samlResponse) {
-        return JwtUtil.generateToken(userService.save(samlResponse).orElse(null), null, this.jwtProperties.getSecret(), "Bearer");
+    @PostMapping
+    public ResponseEntity<String> callback(@RequestBody String samlResponse) {
+        return JwtUtil.generateToken(userService.save(samlResponse).orElse(null), null, this.jwtProperties.getSecret(), "Bearer").map(token -> {
+            return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .header("Location", String.format("https://scp.security.thoughtworks.cn/?token=%s", token))
+                .body("");
+        }).orElse(ResponseEntity
+                .status(HttpStatus.FOUND)
+                .header("Location", "https://opendia.security.thoughtworks.cn")
+                .body("?err='failed to accept saml'"));
     }
 }
